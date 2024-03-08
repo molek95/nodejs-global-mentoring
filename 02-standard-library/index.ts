28d8ff8b044981a5c861b885f400583457a4bf4a
Adam Molnar <Adam_Molnar1@epam.com>
2024-03-08 16:53:20 +0100

    Add inner timer

--- 02-standard-library/index.ts
+++ 02-standard-library/index.ts
@@ -1,26 +1,31 @@
 import { exec } from "node:child_process";
 import { formatLog, getPlatformCommand, logToFile } from "./src/utils";
+import { INTERVAL, ONE_MINUTE_INTERVAL } from "./src/models";
 
-function logActivity(): void {
-  const command = getPlatformCommand();
+const command = getPlatformCommand();
+let logTimer = 0;
 
+setInterval(() => {
+  logActivity();
+}, INTERVAL);
+
+function logActivity() {
   exec(command, (err: Error | null, stdout: string, stderr: string) => {
     if (err) {
       console.log(err.message);
       return;
     }
 
-    const result: string = stdout.trim();
-
+    let result: string = stdout.trim();
     process.stdout.clearLine(-1);
     process.stdout.cursorTo(0);
-
     process.stdout.write(result);
 
-    const log = formatLog(result);
-
-    logToFile(log);
+    logTimer += INTERVAL;
+    console.log({ logTimer });
+    if (logTimer % ONE_MINUTE_INTERVAL === 0) {
+      let log: string = formatLog(result);
+      logToFile(log);
+    }
   });
 }
-
-setInterval(logActivity, 60000);
--- 02-standard-library/src/models/index.ts
+++ 02-standard-library/src/models/index.ts
@@ -1,2 +1,2 @@
 export { Command } from "./commands.enum";
-export { LOG_FILE } from "./logFile.const";
+export { LOG_FILE, INTERVAL, ONE_MINUTE_INTERVAL } from "./activityLog.const";
